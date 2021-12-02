a64fe5b7e3f041eb65e317c523e855b56323e6bb
guilherme <a19guillermobf@iessanclemente.net>
2021-12-03 00:02:22 +0100

    2021-12-03

---
+++ chicles/scripts/myscript.js
@@ -0,0 +1,30 @@
+/***************************************************************************************************************
+La pregunta que nos hacemos es ¿cuántos chicles nos podremos comer según la oferta que haya en ese momento?
+
+Entrada
+Cada caso de prueba está compuesto de una única línea que tiene tres números enteros (no mayores de 109) separados por espacios.
+Los dos primeros números indican cuántos envoltorios se necesitan para que nos den qué cantidad de chicles.
+El tercer número indica el número de chicles que compramos inicialmente. Ninguno de esos números será negativo;
+el primero, además, será siempre mayor que cero. Los casos de prueba terminan cuando los tres son cero.
+
+Salida
+Para cada caso de prueba se escribirá una línea que tendrá, a su vez, dos números. El primero indicará el número total de chicles
+que nos comemos, y el segundo con cuántos envoltorios nos quedamos al final.
+
+Si la oferta hace que la empresa se arruine con nosotros se escribirá RUINA. Se considerará que la empresa va a la ruina cuando
+tenga que regalarnos chicles de forma indefinida.
+
+Entrada de ejemplo
+5 1 25
+5 1 5
+10 1 100
+2 5 20
+0 0 0
+
+Salida de ejemplo
+31 1
+6 1
+111 1
+RUINA 
+***************************************************************************************************************/
+
