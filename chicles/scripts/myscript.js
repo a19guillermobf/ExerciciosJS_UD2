6e62a1a20c67537f82295cde7e2dffea82e793d6
guilherme <a19guillermobf@iessanclemente.net>
2021-12-09 20:14:45 +0100

    2021-12-09 Rematado o exercicio dos chicles

--- chicles/scripts/myscript.js
+++ chicles/scripts/myscript.js
@@ -41,23 +41,6 @@ function lerDato(mensagem){
     return prompt(mensagem);
 }
 
-/** Separa a entrada e executa mentras nom seja 0 0 0 */
-let entrada;
-let chiclescomprados;
-let envoltorios;
-let chiclesregalo;
-
-while(chiclescomprados !== 0 && envoltorios !== 0 && chiclesregalo !== 0){
-    entrada=lerDato("introduze numero de envoltorios | chicles por cada n envoltorios | chicles que compras");
-    entrada=entrada.split(" ");
-    
-    chiclescomprados = parseInt(entrada[2]);
-    envoltorios = parseInt(entrada[0]);
-    chiclesregalo = parseInt(entrada[1]);
-
-    console.log(calcularExemplos(chiclescomprados, envoltorios, chiclesregalo))
-}
-
 /** Entendo que, se dividindo o número de chicles comprados polos envoltorios necesarios para regalar
  * da um número que é mais da metade de chicles comprados daquela vai a ruina
  * 
@@ -65,37 +48,79 @@ while(chiclescomprados !== 0 && envoltorios !== 0 && chiclesregalo !== 0){
  * se o resto da divisom é maior que o número necesario de envoltorios para regalar o chicle, volve-se dividir esse resto, 
  * e imos sumando no resultado final
  */
-function calcularExemplos(chiclescomprados, envoltorios, chiclesregalo){
-    let chiclescomidos = chiclescomprados;
-    let envoltoriosdesobra = 0;
-    let dividendo = chiclescomprados;
-    /** 2 5 20 Este está mal, devera ser a comprovaçom que 20 entre 2
-     * por 5 seja menor que 20
+function calcularExemplos(chiclescomprados, envoltoriosprecisos, chiclesregalo){
+    let chiclescomidos = 0;
+    let chiclessemcomer = chiclescomprados;
+    let chiclesquepodocomer = 0;
+    let chiclesregalados = 0;
+
+    /** Se o número de envoltorios precisos para que che regalem mais chicles é
+     * menor ou igual que os chicles que che regalam de cada vez a empresa vai a quebra
+     * é dizer, se che comprem 5 ou menos envoltorios para que che regalem 5 ou mais chicles
+     * a empresa sempre che vai estar dando chicles, porque sempre vas ter, como mínimo o mesmo
+     * número de envoltorios para que che dem outra vez o mesmo número de chicles.
      */
-    if (chiclescomprados/envoltorios >= chiclescomprados/2){
+    if (envoltoriosprecisos<=chiclesregalo){
         return "RUINA"
     } else {
-        while(dividendo >= envoltorios){
-            /** Nesta volta, cantos chicles me regalam? 
-             * Pois os envoltorios dos chicles que comim polos chiclesregalo 
-             * e sumo-lho a chiclescomidos*/
-
-            chiclescomidos+= Math.floor((dividendo/envoltorios)*chiclesregalo);
-            /** Cantos envoltorios me quedam despois desses chicles que me regalam? 
-             * Pois os chicles que nom comim, mais os chicles que me regalam
-             * Os chicles que nom comim som o resto de dividendo%envoltorios
-             * e os que me regalam (dividendo/envoltorios)*chiclesregalo
+        while(chiclessemcomer >= envoltoriosprecisos){
+            /** Mentras os chicles sem comer que tenho por turno sejam mais que os envoltorios 
+             * precisos para que me regalem mais chicles, vam-me regalar mais chicles polo
+             * que vou ter que seguir calculando
             */
-            envoltoriosdesobra = (dividendo%envoltorios) + Math.floor((dividendo/envoltorios)*chiclesregalo);
 
-            /** Com cantos chicles se vai fazer o próximo calculo? 
-             * Pois igual ca antes?? 
-             * Cos que nom comim mais cos que me regalam?
-            */
-            dividendo = (dividendo%envoltorios) + Math.floor((dividendo/envoltorios)*chiclesregalo);
+            /** Chicles que podo comer em cada turno, que vam ser os justos para que me regalem novos chicles
+             * deixando sem comer o resto que nom daria para cambiar por mais
+             */
+            chiclesquepodocomer = chiclessemcomer - chiclessemcomer%envoltoriosprecisos;
+
+            /** Chicles sem comer atualiza-se, sendo o resto do que vinha anteriormente
+             * entre os envoltorios precisos para regalar mais chicles
+             */
+            chiclessemcomer = chiclessemcomer%envoltoriosprecisos;
+            /** Atualizam-se os chicles comidos cos que podo comer para que me dem mais chicles
+             */
+            chiclescomidos += chiclesquepodocomer;
+            /** Calcula os chicles que regalam desta volta, que vam ser os que podo comer entre os envoltorios precisos,
+             * multiplicado polos chicles de regalo que se estipulam na entrada de datos.
+             * Como já se fixo antes o cálculo, a divisom vai ser exata, polo que já nom se 
+             * redondea
+             */
+            chiclesregalados = (chiclesquepodocomer/envoltoriosprecisos)*chiclesregalo;
+
+            /** Atualiza-se o número de chicles sem comer para a seguinte volta do bucle, engadindo
+             * os chicles que regalarom
+             */
+            chiclessemcomer += chiclesregalados;
         }
-        return `Chicles comidos:${chiclescomidos}, envoltorios de sobra ${envoltoriosdesobra}`
+        /* Umha vez que os chicles sem comer som menores que os envoltorios precisos para que regalem mais chicles, finaliza o bucle
+        e devolvem-se os chicles comidos, que som os comidos mais os sem comer que nom chegam para cambiar por mais
+        e os envoltorios de sobra, que som os chicles sem comer que nom chegam para cambiar por mais */
+        return `Chicles comidos:${chiclescomidos+chiclessemcomer}, envoltorios de sobra ${chiclessemcomer}`
     }
 }
 
+/*6 5 30 agora si que funciona, antes entrava num bucle calquera entrada do estilo n n-1 y>n*/
 
+/** Separa a entrada e executa mentras nom seja 0 0 0 */
+let entrada;
+let chiclescomprados;
+let envoltorios;
+let chiclesregalo;
+
+while(chiclescomprados !== 0 || envoltorios !== 0 || chiclesregalo !== 0){
+    entrada=lerDato(`introduze numero de envoltorios precisos para conseguir mais chicles
+    numero de chicles por cada n envoltorios precisos e 
+    chicles que compras inicialmente separados por espaços
+    Ex: 6 5 30`);
+    entrada=entrada.split(" ");
+    
+    chiclescomprados = parseInt(entrada[2]);
+    envoltorios = parseInt(entrada[0]);
+    chiclesregalo = parseInt(entrada[1]);
+    if (chiclescomprados===0 && envoltorios===0 && chiclesregalo===0){
+        console.log("Execuçom finalizada")
+    } else {
+        console.log(calcularExemplos(chiclescomprados, envoltorios, chiclesregalo))
+    }
+}
