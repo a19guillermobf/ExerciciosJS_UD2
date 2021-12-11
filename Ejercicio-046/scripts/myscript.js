246fba7637cf358502fd56a665480f979bddb614
guilherme <a19guillermobf@iessanclemente.net>
2021-12-11 18:33:21 +0100

    2021-12-11 Rematado o exercicio 46, mais ou menos

--- Ejercicio-046/scripts/myscript.js
+++ Ejercicio-046/scripts/myscript.js
@@ -19,4 +19,32 @@ function longitudeArray(mensagem){
     return longitude;
 }
 
+/** Crea um novo array de longitude n e com números aleatorios do 0 ao 9 */
+function creaArray(n){
+    let novoarray=[]
+    for(let i=n; i>0;i--){
+        /** Crea um numero aleatorio do 0 ao 9 */
+        novoarray.push(Math.floor(Math.random()*10));
+    }
+    return novoarray;
+}
 
+/** Suma dous arrays, supom-se que vam ser vectores numéricos */
+function sumaVectores(arr1, arr2){
+    console.log(arr1);
+    console.log(arr2);
+    let arrayfinal=[];
+    if (arr1.length !== arr2.length){
+        return false;
+    } else {
+        for(let i = 0; i<arr1.length; i++){
+            arrayfinal.push(arr1[i]+arr2[i]);
+        }
+        return arrayfinal;
+    }
+}
+
+let longitude = longitudeArray();
+
+let arrayfinal = sumaVectores(creaArray(longitude),creaArray(longitude));
+console.log(arrayfinal);
